ac0411645bea3400ce648261fcca0c07d95ae40f
Masaya Tojo <masaya@tojo.tokyo>
2022-03-19 04:36:37 +0900

    info.rkt, more-type-safe.rkt: Update quotient and modulo types.

--- info.rkt
+++ info.rkt
@@ -1,6 +1,6 @@
 #lang info
 
 (define deps '("typed/racket"))
-(define version "0.1.0")
+(define version "0.1.1")
 (define pkg-desc "Make type/racket language more type safe")
 (define license '(MIT))
